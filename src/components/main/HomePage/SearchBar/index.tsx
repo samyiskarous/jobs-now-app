e345af94e6441879400802be9ee1f22e28fa7a34
Samy <samygeorge007@gmail.com>
2021-05-29 15:05:38 +0200

    feat: Handeled redirecting to /search upon searching with more than 3 chars

--- src/components/main/HomePage/SearchBar/index.tsx
+++ src/components/main/HomePage/SearchBar/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import { connect } from 'react-redux';
+import { Redirect, useHistory, useLocation } from 'react-router';
 import './styles.css'
 
 const SearchInput = (props: any) => {
+
+    const history = useHistory();
+
+    const searchHandler = (searchText: string) => {
+        if(searchText.length > 3){
+            history.push('/search');
+        }
+    }
+
     return (
         <div className="searchBarContainer">
-            <input type="text" placeholder="Search Keyword" className="searchInput"/>
+            <input 
+                type="text" 
+                placeholder="Search Keyword" 
+                className="searchInput"
+                onChange={event => searchHandler(event.target.value)}
+                />
         </div>
     );
 }
